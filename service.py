c4a3a5ac3c24ba9d87913660152b31ef3706b558
Anastasia Sledgianowski <ani.sledge@gmail.com>
2022-05-09 22:30:27 -0600

    Fix sqlite3 insert syntax, file open modes

--- service.py
+++ service.py
@@ -33,15 +33,15 @@ def message_table_exists(database):
     """
     try: 
         cursor = database.cursor()
-        row = cursor.execute('''SELECT * FROM sqlite_schema
-                          WHERE name="messages";''')
+        row = cursor.execute('''SELECT name FROM sqlite_master 
+                          WHERE name='messages';''').fetchone()
         database.commit()
 
-        if len(row) > 0:
+        if row is not None:
             return True
     
-    except:
-        print("Exception in message_table_exists")
+    except sqlite3.Error as e:
+        print("Exception in message_table_exists", e.args[0])
     
     return False
 
@@ -57,7 +57,6 @@ def create_message_table(database):
     try:
         cursor = database.cursor()
         cursor.execute('''CREATE TABLE IF NOT EXISTS messages (
-                        id INTEGER PRIMARY KEY AUTOINCREMENT,
                         text VARCHAR(150) NOT NULL);''')
         database.commit()
         return True
@@ -89,8 +88,8 @@ def insert_samples(database):
         database.commit()
         return True
 
-    except:
-        print("Exception in insert_samples")
+    except sqlite3.Error as e:
+        print("Exception in insert_samples", e.args[0])
         return False
 
 def get_count(database):
@@ -108,8 +107,8 @@ def get_count(database):
         count = cursor.execute("SELECT COUNT(text) FROM messages;").fetchone()[0]
         database.commit()
     
-    except:
-        print("Exception in get_count.")
+    except sqlite3.Error as e:
+        print("Exception in get_count.", e.args[0])
     
     return count
 
@@ -126,12 +125,12 @@ def get_message(database):
         cursor = database.cursor()
         message = cursor.execute('''SELECT text FROM messages
                                     ORDER BY RANDOM()
-                                    LIMIT 1;''')[0]
+                                    LIMIT 1;''').fetchone()
         database.commit()
-        return message
+        return message[0]
 
-    except:
-        print("Exception in get_message.")
+    except sqlite3.Error as e:
+        print("Exception in get_message.", e.args[0])
 
     return message
 
@@ -147,12 +146,12 @@ def create_message(text, database):
     """    
     try:
         cursor = database.cursor()
-        cursor.execute("INSERT INTO messages VALUES (?);", text)
+        cursor.execute("INSERT INTO messages VALUES (?);", [(text)])
         database.commit()
         return True
 
-    except:
-        print("Exception in create_message")
+    except sqlite3.Error as e:
+        print("Exception in create_message", e.args[0])
         return False
 
 def main():
@@ -181,26 +180,40 @@ def main():
     print("The Random Message Generator Microservice is running.")
     print("Enter 'Ctrl-C' to exit.")
 
-    with open('./coms.txt', 'w+') as comsFile:
+    while True:
+
+        command = None
+        with open('./coms.txt', 'r') as comsFile:
+            command = comsFile.read()
         
-        with open('./message.txt') as messageFile: 
+        print("Command: ", command)
 
-            while True:
-                command = comsFile.readline()
+        if command == "GET":
 
-                if command == "GET":
-                    message = get_message(database)
+            message = get_message(database)
 
-                    if message is not None:
-                        comsFile.write("OK")
+            if message is not None:
+                print("Message retrieved:", message)
 
-                else if command == "POST":
-                    text = messageFile.readline()
-
-                    if create_message(text, database):
-                        comsFile.write("OK")
+                with open('./message.txt', 'w') as messageFile:
+                    messageFile.write(message)
                 
-                time.sleep(5)
+                with open('./coms.txt', 'w') as comsFile:
+                    comsFile.write("OK")
+
+        elif command == "POST":
+
+            text = None
+            with open('./message.txt', 'r') as messageFile:
+                text = messageFile.read()
+
+            if create_message(text, database):
+                print("Message created:", text)
+
+                with open('./coms.txt', 'w') as comsFile:  
+                    comsFile.write("OK")
+        
+        time.sleep(5)
     
     return 0
 
